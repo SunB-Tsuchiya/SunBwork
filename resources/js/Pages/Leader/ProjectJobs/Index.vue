817d1e8dba6406b5a4b738a1b10bb4ce454902a2
SunB-Tsuchiya <h-tsuchiya@suna.co.jp>
2026-04-02 22:48:09 +0900

    fix(leader): 案件総覧テーブル列幅を全ピクセル指定に統一（比率ベースに修正）

--- resources/js/Pages/Leader/ProjectJobs/Index.vue
+++ resources/js/Pages/Leader/ProjectJobs/Index.vue
@@ -72,14 +72,14 @@
                         <span class="ml-2 text-xs font-normal text-orange-500">{{ group.items.length }} 件</span>
                     </div>
 
-                    <table class="w-full table-fixed border" style="min-width: 840px;">
+                    <table class="w-full table-fixed border" style="min-width: 500px;">
                         <colgroup>
-                            <col style="width: 117px"> <!-- 伝票番号 -->
-                            <col style="width: 117px"> <!-- 登録日 -->
-                            <col style="width: 60%">   <!-- 案件名 -->
-                            <col style="width: 15.6%"> <!-- クライアント -->
-                            <col style="width: 10%">   <!-- 担当Co -->
-                            <col style="width: 9%">    <!-- ステータス -->
+                            <col style="width: 60px">  <!-- 伝票番号 -->
+                            <col style="width: 60px">  <!-- 登録日 -->
+                            <col style="width: 160px"> <!-- 案件名 -->
+                            <col style="width: 90px">  <!-- クライアント -->
+                            <col style="width: 60px">  <!-- 担当Co -->
+                            <col style="width: 44px">  <!-- ステータス -->
                         </colgroup>
                         <thead>
                             <tr class="bg-gray-50">
